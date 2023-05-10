3a51600f3ee65fd30f573b2752d4fd387bbd894b
Max Yeap <max@yongtuck.com>
2023-05-10 19:25:30 +0800

    Change sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 
 # Define an array of airports with city names and codes
 airports = [
+  { code: 'SIN', city: 'Singapore' },
   { code: 'KUL', city: 'Kuala Lumpur' },
-  { code: 'JFK', city: 'New York' },
-  { code: 'LAX', city: 'Los Angeles' },
-  { code: 'ORD', city: 'Chicago' },
-  { code: 'DFW', city: 'Dallas' },
-  { code: 'DEN', city: 'Denver' },
-  { code: 'MIA', city: 'Miami' },
-  { code: 'SFO', city: 'San Francisco' },
-  { code: 'LAS', city: 'Las Vegas' },
-  { code: 'CLT', city: 'Charlotte' },
-  { code: 'ATL', city: 'Atlanta' }
+  { code: 'BKK', city: 'Bangkok' },
+  { code: 'HKG', city: 'Hong Kong' },
+  { code: 'MNL', city: 'Manila' },
+  { code: 'CGK', city: 'Jakarta' },
+  { code: 'HAN', city: 'Hanoi' },
+  { code: 'DPS', city: 'Denpasar' },
+  { code: 'PNH', city: 'Phnom Penh' },
+  { code: 'RGN', city: 'Yangon' },
+  { code: 'KTM', city: 'Kathmandu' }
 ]
 
 # Loop through the array and create an Airport object for each code and city
@@ -29,23 +29,20 @@ end
 # Get an array of all airport IDs
 airport_ids = Airport.pluck(:id)
 
-# Loop 10 times to create 10 flights
-100.times do
-  # Get two random airport IDs that are not the same
-  departure_id = airport_ids.sample
-  arrival_id = airport_ids.reject { |id| id == departure_id }.sample
+# Loop through the airport_ids to create flights
+airport_ids.each do |departure_id|
+  airport_ids.each do |arrival_id|
+    next if departure_id == arrival_id # skip if departure and arrival airport are the same
 
-  # Generate a random datetime object for the start time (between 1 hour and 7 days from now)
-  start_time = Time.current + rand(1.hour..30.days)
-
-  # Generate a random duration between 1 and 6 hours
-  duration = rand(180..840)
-
-  # Create the flight with the generated attributes
-  Flight.create!(
-    departure_airport_id: departure_id,
-    arrival_airport_id: arrival_id,
-    start: start_time,
-    duration:
-  )
+    10.times do |i|
+      start_time = Time.current + i.days + rand(1.hour..7.days) # start_time is between 1 hour and 7 days from now
+      duration = rand(180..840) # duration is between 3 and 14 hours
+      Flight.create!(
+        departure_airport_id: departure_id,
+        arrival_airport_id: arrival_id,
+        start: start_time,
+        duration:
+      )
+    end
+  end
 end
